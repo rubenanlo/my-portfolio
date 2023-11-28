147a5b7ef7d49edb446fc23a15af5b1587ffe191
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-11-28 17:45:46 +0100

    Updated hero section

--- components/Hero.js
+++ components/Hero.js
@@ -1,59 +1,73 @@
 import Image from "next/image";
-import { ChevronRightIcon } from "@heroicons/react/20/solid";
-import { Container } from "./Container";
+import { Container } from "components/Container";
+import { TextLayout } from "components/TextLayout";
 import StarsCanvas from "components/Stars";
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from "components/SocialIcons";
 import { RUBEN_HEADSHOT as profilePic } from "helpers/exportImages";
 
-export default function Example() {
+const text = {
+  name: "Ruben Andino",
+  title: "Fullstack Web Developer, Economist",
+  social: [
+    {
+      social: GitHubIcon,
+      href: "https://github.com/rubenanlo",
+    },
+    {
+      social: LinkedInIcon,
+      href: "https://www.linkedin.com/in/ruben-andino/",
+    },
+    {
+      social: TwitterIcon,
+      href: "https://twitter.com/rubenanlo",
+    },
+    {
+      social: InstagramIcon,
+      href: "https://www.instagram.com/benjiebao/",
+    },
+    {
+      text: "Check my other site",
+      href: "https://www.rawdev.io/",
+    },
+  ],
+};
+
+const Hero = () => {
   return (
-    <Container className="relative isolate overflow-hidden">
-      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
-        <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+    <Container className="relative isolate">
+      <Container.Flex className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:px-8 lg:py-40">
+        <Container className="mt-10 pl-20 mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
           <Image
-            className="h-20 w-20 rounded-full"
+            className="h-16 w-16 rounded-full dark:sepia"
             src={profilePic}
-            alt="Your Pic"
+            alt="My Pic"
           />
-          <div className="mt-24 sm:mt-32 lg:mt-16">
-            <a href="#" className="inline-flex space-x-6">
-              <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
-                Whats new
-              </span>
-              <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-                <span>Just shipped v1.0</span>
-                <ChevronRightIcon
-                  className="h-5 w-5 text-gray-500"
-                  aria-hidden="true"
+          <TextLayout className="mt-5">
+            <TextLayout.Title title={text.name} />
+            <TextLayout.Subtitle subtitle={text.title} />
+            <TextLayout.Paragraph paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate." />
+            <Container.Flex className="items-center gap-x-6">
+              {text.social.map(({ social, href, text }) => (
+                <Container.Link
+                  key={social || text}
+                  href={href}
+                  target="_blank"
+                  Component={social}
+                  text={text}
                 />
-              </span>
-            </a>
-          </div>
-          <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Deploy to the cloud with confidence
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
-          </p>
-          <div className="mt-10 flex items-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-            >
-              Get started
-            </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-white">
-              Learn more <span aria-hidden="true">→</span>
-            </a>
-          </div>
-        </div>
-        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <StarsCanvas />
-          </div>
-        </div>
-      </div>
+              ))}
+            </Container.Flex>
+          </TextLayout>
+        </Container>
+        <StarsCanvas triggerRotation />
+      </Container.Flex>
     </Container>
   );
-}
+};
+
+export default Hero;
---
+++ components/TextLayout.js
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+
+export const TextLayout = ({ className, children }) => (
+  <div className={clsx(className, "w-full text-zinc-500 dark:text-gray-200")}>
+    {children}
+  </div>
+);
+
+TextLayout.Title = function TextLayoutTitle({ title, className }) {
+  return (
+    <h1 className={clsx(className, "text-4xl font-bold sm:text-5xl")}>
+      {title}
+    </h1>
+  );
+};
+
+TextLayout.Subtitle = function TextLayoutSubtitle({ subtitle, className }) {
+  return <h2 className={clsx(className, "text-2xl leading-8 ")}>{subtitle}</h2>;
+};
+
+TextLayout.Paragraph = function TextLayoutParagraph({ paragraph, className }) {
+  return <p className={clsx(className, "mt-6 text-gray-400")}>{paragraph}</p>;
+};
