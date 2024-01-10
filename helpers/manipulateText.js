cbbfd7501a3429904773f23382296d3f3f679baf
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-01-10 15:39:36 +0100

    Added new helper to manipulate text

---
+++ helpers/manipulateText.js
@@ -0,0 +1,5 @@
+export const turnObjectIntoString = (className) => {
+  if (className === undefined) return;
+  if (typeof className === "string") return className;
+  return Object.values(className).toString().replaceAll(",", " ");
+};
