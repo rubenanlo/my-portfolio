0d2703bc1aee774905d67579be84d7ed97ad6dd7
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-01-18 16:59:46 +0100

    Updated setUniqueId helper

--- helpers/setUniqueId.js
+++ helpers/setUniqueId.js
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from "uuid";
 // This helper allows to add unique Ids to arrays, so that we can guarantee
 // unique keys in the different components
 export const setUniqueId = (array) =>
-  array &&
   array.map((element) =>
     typeof element === "string"
       ? { paragraph: element, id: uuidv4() }
