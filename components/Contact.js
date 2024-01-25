96d1837479a41b62788ba5f1042d3f5e2fd4002f
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-01-25 12:07:45 +0100

    Added provisional contact me button in homepage

--- components/Contact.js
+++ components/Contact.js
@@ -1,18 +1,25 @@
 import { Container } from "components/Container";
 import { Button } from "components/Button";
-import { Form } from "components/Form";
+// import { Form } from "components/Form";
 import { MailIcon } from "components/AppIcons";
 import { TextLayout } from "components/TextLayout";
 
 // !Create backend for this section (modal confirming email and sending email)
 const Contact = () => (
-  <Form
-    action="/thank-you"
+  <Container
     className={{
       border: "border border-zinc-100 p-6 dark:border-zinc-700/40",
       otherStyles: "rounded-2xl",
     }}
   >
+    {/* <Form
+    // action="/thank-you"
+    className={{
+      border: "border border-zinc-100 p-6 dark:border-zinc-700/40",
+      otherStyles: "rounded-2xl",
+    }}
+  > */}
+
     <TextLayout.Title
       as="h4"
       className="flex"
@@ -24,7 +31,7 @@ const Contact = () => (
       paragraph="Get notified when I publish something new, and unsubscribe at any time."
     />
     <Container.Flex className="mt-6">
-      <Form.Field
+      {/* <Form.Field
         variant="primary"
         field="email"
         type="email"
@@ -37,9 +44,18 @@ const Contact = () => (
         type="submit"
         className="ml-4 flex-none"
         text="Join"
-      />
+      /> */}
+      <Container.Link href="mailto:randinocv@gmail.com">
+        <Button
+          variant="primary"
+          type="submit"
+          className="flex-none"
+          text="Contact me"
+        />
+      </Container.Link>
     </Container.Flex>
-  </Form>
+    {/* </Form> */}
+  </Container>
 );
 
 export default Contact;
