d02447a4f7e82ad84757025f0c1006e9de3d4c65
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-01-29 13:54:35 +0100

    updated meta info

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ export const ICON = "static/assets/favicon.ico";
 
 // The URL of the deployed page (used in meta tags for SMO)
 // Example: https://www.domain.com/
-export const URL = "https://afolumitigationinventory.org";
+export const URL = "https://ruben-andino.rawdev.io";
 
 // The URL of the CV (Excel)
 // Example: /static/downloads/files/SDR2021-database.xlsx
@@ -16,13 +16,15 @@ export const CV_DOWNLOAD_URL = "/static/downloads/2023-mitigation-data.xlsx";
 
 // The description shown in Google Search results and on social media networks
 // Should be max ~150 characters.
-export const META_DESCRIPTION = "TODO";
+export const META_DESCRIPTION =
+  "Welcome to my personal site, where you can learn about my work and read my blog posts.";
 
 // The preview image shown in social networks and when sharing on messengers,
 // like WhatsApp and others
 // The URL should be absolute, e.g.: https://www.domain.com/static/preview.jpg
 // Recommended size: 1200 x 627
-export const META_IMAGE = "TODO";
+export const META_IMAGE =
+  "https://ruben-andino.rawdev.io/public/assets/my_portfolio.png";
 
 // The Google Analytics tracking ID
 export const GA_TRACKING_ID = "TODO";
